8965f76cdbf3451f69cc38aca4b8855e409f8ef8
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2025-12-15 23:08:34 +0700

    Update lab7_2.cpp

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,17 +2,51 @@
 
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
+?????: Etheri
 Fahsai: Wow!!! Luke Skywalker is a really cool name.
 Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
+Etheri: 680610496
+Fahsai: I think you may be GEAR 56. I have a free movie ticket for you.
 Fahsai: Let's go to the cinema together!!!
 Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
+Etheri: Star Wars VII
 Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
+Etheri: Next Monday
 Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
+Etheri: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string Goh;
+    int dex;
+    string Mew;
+    string Castform;
+    string Quotes;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n?????: ";
+    getline(cin, Goh);
+    cout << "Fahsai: Wow!!! " << Goh << " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. ";
+    cout << "What is your student ID?\n" << Goh <<": ";
+    cin >> dex;
+    cout << "Fahsai: I think you may be GEAR "<< int(dex)/10000000 - 12 <<". ";
+    cout << "I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << Goh <<": ";
+    getline(cin >> ws, Mew);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << Goh <<": ";
+    getline(cin, Castform);
+    cout << "Fahsai: ";
+    cout << Castform << "....that is OK!!! I'm looking forward to watching "<< Mew <<" with you.\n";
+    cout << Goh <<": ";
+    getline(cin, Quotes);
+    cout << "Fahsai: 555+ see you "<< Castform << ". Bye Bye \\(^ ^)/";
+    return 0;
+}
